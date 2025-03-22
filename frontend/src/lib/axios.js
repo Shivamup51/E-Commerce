b4ff2dc7b272e87bf0c8ea8c1777a684971ec44e
Shivamup51 <shivamupadhyay2868@gmail.com>
2025-03-22 16:24:48 +0530

    Update axios instance to use the production API base URL for deployment

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-	baseURL: import.meta.env.VITE_API_URL + "/api",
+	baseURL: "https://e-commerce-backend-tau-ten.vercel.app" + "/api",
 	withCredentials: true, // send cookies to the server
 });
 
